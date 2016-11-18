8eedb98f2faf868147de22c9fb33fc8ed629cee5
Madalin <madalinnitu96@yahoo.ro>
2016-11-18 21:30:51 +0200

    Serialize and Deserialize XML Test

--- University Schedule/Test.cs
+++ University Schedule/Test.cs
@@ -5,31 +5,107 @@ using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 using System.Xml.Serialization;
 
 namespace University_Schedule
 {
-
-  
-
+    /// <summary>
+    /// Trebuie o clasa Orar care sa cuprinda - zile care zile cuprind cursurile pe ore
+    /// 
+    /// Orar -> Zile(5) ( Luni,Marti,Miercuri,Joi,Vineri) -> Fiecare contin Cursuri-> Fiecare Curs contine
+    ///                                 (Materia,Nume Prof,Sala,Semigrupa(poate sa fie omisa),si orele dupa care vor fi sortate cand sunt introduse in zi)
+    ///       !!! Astfel in Zile Cursurile vor fi in ordine gen curs ora 8-10 urmat de Curs ora 14-16 daca nu exista nimic intre ele.
+    /// </summary>
     public partial class Test : Form
     {
         public Test()
         {
             InitializeComponent();
-            Test_s a = new Test_s();
-            a.Name = "Bob";
-            a.Age = 33;
-            a.XmlWriter("heyy.xml");
 
-            Test_s b = new Test_s();
-            b.Name = "Adam";
-            b.Age = 23;
-            b.XmlWriter("heyy.xml");
+
+
+            //Test_s a = new Test_s();
+            //a.Name = "Braien";
+            //a.Age = 24;
+            //a.newstruct = new newStruct("dd",3);
+
+            //Test_s b = new Test_s();
+            //b.Name = "Adam";
+            //b.Age = 23;
+
+            ////SerializeToXmlFile(a, "new.xml", typeof(Test_s));
+            ////SerializeToXmlFile(b, "new.xml", typeof(Test_s));
+            //List<Test_s> l = new List<Test_s>();
+            //l.Add(a);
+            //l.Add(b);
+            //SerializeToXmlFile(l, "new.xml");
+
+
+            DeserializeToXmlFile();
         }
+
+        public static void SerializeToXmlFile(object obj,string file,Type type)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(type);
+
+            using (var textWriter = new StreamWriter(file))
+            {
+                xmlSerializer.Serialize(textWriter, obj);
+            }
+        }
+        public static void SerializeToXmlFile(List<Test_s> list,string filename)
+        {
+            XmlSerializer serializer = new XmlSerializer(list.GetType());
+            using (TextWriter writer = new StreamWriter(filename))
+            {
+                serializer.Serialize(writer, list);
+            }
+        }
+        [XmlRoot("StepList")]
+        public class StepList
+        {
+            [XmlElement("Step")]
+            public List<Step> Steps { get; set; }
+        }
+
+        public class Step
+        {
+            [XmlElement("Name")]
+            public string Name { get; set; }
+            [XmlElement("Desc")]
+            public string Desc { get; set; }
+        }
+        public static void DeserializeToXmlFile()
+        {
+            StepList result;
+            string testData = @"<StepList>
+                        <Step>
+                            <Name>Name1</Name>
+                            <Desc>Desc1</Desc>
+                        </Step>
+                        <Step>
+                            <Name>Name2</Name>
+                            <Desc>Desc2</Desc>
+                        </Step>
+                    </StepList>";
+
+            XmlSerializer serializer = new XmlSerializer(typeof(StepList));
+            using (TextReader reader = new StringReader(testData))
+            {
+                 result = (StepList)serializer.Deserialize(reader);
+            }
+            foreach(var i in result.Steps)
+            {
+                MessageBox.Show(i.Name+ "   "+ i.Desc);
+            }
+        }
+
     }
    
 
--- University Schedule/Test_s.cs
+++ University Schedule/Test_s.cs
@@ -9,54 +9,23 @@ using System.Xml.Serialization;
 
 namespace University_Schedule
 {
+    public struct newStruct
+    {
+        public string name;
+        public int data;
+        public newStruct(string n,int d)
+        {
+            name = n;
+            data = d;
+        }
+    }
     public class Test_s
     {
         public string Name { get; set; }
         public int Age { get; set; }
-      
-        public void XmlWriter(string filename)
-        {
-            #region On Error Retry and retrycount with xml file
-            
-            try
-            {
-                if (!File.Exists(filename))
-                {
-                    XmlSerializer xSeriz = new XmlSerializer(typeof(Test_s));
-                    FileStream fs = File.Open(filename, FileMode.Append, FileAccess.Write, FileShare.ReadWrite);
-                    xSeriz.Serialize(fs, this);
-                }
-                else
-                {
-                    XmlDocument doc = new XmlDocument();
-                    doc.Load(filename);
 
-                    XmlNode xnode = doc.CreateNode(XmlNodeType.Element, "New", null);
-                    XmlSerializer xSeriz = new XmlSerializer(typeof(Test_s));
-                    XmlSerializerNamespaces ns = new XmlSerializerNamespaces();
-                    ns.Add("", "");
-                    XmlWriterSettings writtersetting = new XmlWriterSettings();
-                    writtersetting.OmitXmlDeclaration = true;
-                    StringWriter stringwriter = new StringWriter();
-                    using (XmlWriter xmlwriter = System.Xml.XmlWriter.Create(stringwriter, writtersetting))
-                    {
-                        xSeriz.Serialize(xmlwriter, this, ns);
-                    }
-                    xnode.InnerXml = stringwriter.ToString();
-                    XmlNode bindxnode = xnode.SelectSingleNode("Test_s");
-                    doc.DocumentElement.AppendChild(bindxnode);
-    
-                
-                    doc.Save(filename);
-                }
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-
-            #endregion
-        }
+        public newStruct newstruct { get; set; }
+       
     }
 
 
