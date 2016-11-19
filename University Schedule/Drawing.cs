ea2f07c2c5fed07159400207520c89b25293dd08
Madalin <madalinnitu96@yahoo.ro>
2016-11-19 12:41:36 +0200

    Finaly

--- University Schedule/Drawing.cs
+++ University Schedule/Drawing.cs
@@ -12,12 +12,8 @@ namespace University_Schedule
     {
         Drawing() { }
 
-        /// <summary>
-        /// Mai trebuie sa impartim iar in dreptungiuri mai fine pentru saptamani pare si impare precum si pentru fiecare ora.
-        /// </summary>
-        /// <param i = "Width">Contorizeaza pe verticala la ore 120 fiind lungimea unui patratel la ore</param>
-        /// <param j="Height">Contorizeaza pe origontala 90 fiind lungimea unui patratel la zile</param>
-        /// <returns></returns>
+        public static int index;
+      
         public static Bitmap DrawRectangleOnImage(int Width,int Height)
         {
             Bitmap img = DrawFilledRectangle(Width, Height, Brushes.White);
@@ -40,28 +36,25 @@ namespace University_Schedule
             return img;
         }
 
-        /// <summary>
-        /// Trebuie caz special pentru fiecare situatie : 
-        /// 1) este curs deci are 8 patratele.
-        /// 2) este laborator ori curs odata la 2 saptamani deci 4 patratele
-        /// 3) este o data la 2 saptamani si este laborator de 2 patratele
-        ///  .... Trebuie o formula matematica care sa dea direct incepului dreptunghiului in care sa scrie
-        ///  ........ la fel si pentru celelelalte date.
-        ///  EXEMPLU de dreptunghi care returneaza. 
-        ///  0,0    0,1     0,2     0,3   Pointul de start va fii (0,0) iar indexul va avea numarul de patrate din interior asa ca 
-        ///  1,0    1,1     1,2     1,3     sizeul ca trebui sa fie dimensiunea unui patratel ori indexul.
-        ///  2,0    2,1     2,2     2,3   Adaug la Point-ul start x+35 si y+15 ca sa il mut pe mijloc in cazul laboratorului o data pe saptamana de 4 patratele.
-        ///  3,0    3,1     3,2     3,3   astfel punctul de unde porneste dreptunghiul returnat pentru scriere este de 1,1 aproximativ.
-        /// </summary>
-        /// <param name="start"></param>
-        /// <param name="index"></param>
-        /// <returns></returns>
+      
         public static Rectangle SearchTypeCourse(Point start,int index)
         {
             return (new Rectangle(new Point(start.X +35,start.Y+15), new Size(index * 94, index * 32)));
         }
 
       
+        public static Rectangle[] DivideInRectangle(Rectangle original)
+        {
+            index = 0;
+            Rectangle[] rec = new Rectangle[240];
+            for(int i = original.X;i<=original.Width; i+=94)
+            for(int j = original.Y;j<=original.Height;j+=32)
+                {
+                    rec[index] = new Rectangle(i, j, 94, 32);
+                    index++;
+                }
+            return rec;
+        }
 
         public static Point SearchForMatch(int x,int y)
         {
@@ -93,33 +86,25 @@ namespace University_Schedule
             }
             return myImage;
         }
-        /// <summary>
-        /// Trebuie schimbat dimensiunea font-ului dupa curs astfel intr-un curs de un sfert de patratica sa scrie mic,
-        /// iar intr-unul de curs saptamanar sa scrie font mare ... dimensiunea se schimba in 'new Font("Segoe", AICI)'.
-        /// SFAT : Dimensiunea fontului sa fie trimisa prin parametru in momentul in care se vede de cate dreptungiuri sunt in stiva.
-        /// </summary>
-        /// <param name="text"></param>
-        /// <param name="image"></param>
-        /// <param name="rectf"></param>
-        /// <returns></returns>
+       
 
         public static Bitmap DrawString(string text,Image image,RectangleF rectf)
         {
             Bitmap bmp = new Bitmap(image);
-            //incarcam imaginea intr-un Graphics pentru a desena
+            
             Graphics g = Graphics.FromImage(bmp);
-            //pentru claritate proprietati
+            
             g.SmoothingMode = SmoothingMode.AntiAlias;
             g.InterpolationMode = InterpolationMode.HighQualityBicubic;
             g.PixelOffsetMode = PixelOffsetMode.HighQuality;
-            //functia asta va scrie mesajul din primul parametru in imagine la coordonatele dreptunghiului
+
             g.DrawString(text, new Font("Segoe", 10), Brushes.Black, rectf);
 
-            //curata streamul
+         
             g.Flush();
 
 
-            //returneaza imaginea cu mesajul scris in ea
+           
             return bmp;
         }
 
--- University Schedule/Form Orar Nou.cs
+++ University Schedule/Form Orar Nou.cs
@@ -11,83 +11,63 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 /// <summary>
-/// Posibil inlocuirea array-ului Point si RectangleF cu 2 liste cu aceste date pentru 
-///    manipularea mai buna in scrierea lor in bitmap.
+/// Mai trebuie lucrat putin la Searchul patratelor ca sa fie mai fina.
 /// </summary>
 
 namespace University_Schedule
 {
     public partial class Form_Orar_Nou : Form
     {
-        public Course curs; //pentru stocarea datelor unei selectari ( trebui facut un vector sau introdus in baza de date).
-        Bitmap bmp; //imaginea de manevra ... trebuie abdatat pictureboxul pentru a se vedea stuff-urile adaugate.
-        RectangleF[] rec; //vector pentru stocarea dreptunghiurilor pentru scris ... pentru test este folosit cel dupa pozitia 0.
-        Point[] point; // vector folosit pentru stocarea patratelor pentru colorat in diferite culori ... trebuie facuta o functie de generat culori random.
-        int indexPoint; // indexul pentru cei 2 vectori
-        Rectangle selected_rectagle; //pentru selectarea mai multor patratele
+        public Course curs; 
+        Bitmap bmp; 
+        Point[] point; 
+        int indexPoint;
+        Rectangle selected_rectagle; 
 
         public Form_Orar_Nou()
         {
             InitializeComponent();
-            rec = new RectangleF[20]; //initializare
-            point = new Point[20]; //initializare  ...test 20 se va mari mai tarziu
-            indexPoint = 0; //initializare
-            //se face imaginea initiala si se initializeaza pictureboxul
+            point = new Point[20]; 
+            indexPoint = 0; 
             bmp = Drawing.DrawRectangleOnImage(pictureBox1.Size.Width, pictureBox1.Size.Height);
             pictureBox1.Image = bmp;
         }
-        /// <summary>
-        /// Se stocheaza Point - urile pentru desenarea lor ulterioara... stocharea se face
-        /// direct in dreptunghiurile corecte.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
+        
         private void pictureBox1_Click(object sender, EventArgs e)
         {
             MouseEventArgs me = e as MouseEventArgs;
             Point coordinates = me.Location;
             MessageBox.Show(coordinates.X.ToString() + "   " + coordinates.Y.ToString());
            
-            //se cauta pe baza coordonatelor unde sa dat click in care patrat se doreste colorarea
-            //trebuie updatat cu patrate mai fine pentru cursurile cu ore impare precum si saptamani impare si pare.
-            //point[indexPoint] = Drawing.SearchForMatch(coordinates.X, coordinates.Y);
-
-            //Dreptunghiul unde se va scrie informatia
-          // rec[indexPoint] = new RectangleF(point[indexPoint], new SizeF(94, 32));
-
-         //   indexPoint++;
+           
         }
 
         private void DrawAllRectanglesSelected()
         {
-            // se deseneaza toate dreptunghiurile selectate pentru inserarea datelor.
             for (int i = 0; i < indexPoint; i++)
             {
                 bmp = Drawing.FillRectangleWithAColor(point[i], new Size(94, 32), bmp, Brushes.Red);
+                
             }
-            //update ( nu e necesara decat pentru test ) 
             pictureBox1.Image = bmp;
         }
    
 
-        /// <summary>
-        /// In momentul in care apesi pe buton va scrie textul din Materie in imagine la inceputul ei.
-        /// Trebuie lucrat si la incadrarea intr-un dreptunghi corespunzator.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
         private void button1_Click(object sender, EventArgs e)
         {
-            //trebuie modificat pentru toate datele precum si incadrarea lor in urmatoarele dreptunghiuri.
-           // DrawAllRectanglesSelected();
             curs = new Course();
             var form = new Insert_Course(curs);
             form.ShowDialog();
+            selected_rectagle = new Rectangle(pointStart, new Size(endPoint));
+            using (Graphics graph = Graphics.FromImage(bmp))
+            {
+                graph.FillRectangle(Brushes.Red, selected_rectagle);
+            }
+            
             if (form.DialogResult == DialogResult.OK)
             {
-                ///Rectangle-ul in care scrie este facut din point[0] care e primul point verificat si ultimul 
-                //// Trebuie facut in functia pentru SearchTypeCourse facut pentru fiecare in functie de curs
                 bmp = Drawing.DrawString(curs.Materia,pictureBox1.Image, new RectangleF(point[0], new SizeF(indexPoint * 94, indexPoint * 32)));
+             
             }
             pictureBox1.Image = bmp;
             indexPoint = 0;
@@ -96,20 +76,19 @@ namespace University_Schedule
         }
 
 
-        //o sa ne trebuiasca pentru optimizare
+    
         protected override void OnLoad(EventArgs e)
         {
             base.OnLoad(e);
         }
 
-        /// <summary>
-        /// Pentru selectarea unei parti din picture box tot ce e mai jos e necesar.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
+        
         private Point RectStartPoint;
+        private Point pointStart;
+        private Point endPoint;
         private Rectangle Rect = new Rectangle();
         private Brush selectionBrush = new SolidBrush(Color.FromArgb(128, 72, 145, 220));
+        private bool isDrawing;
 
         private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
         {
@@ -120,6 +99,14 @@ namespace University_Schedule
                     Debug.WriteLine("Right click");
                 }
             }
+            isDrawing = false;
+            endPoint = Drawing.SearchForMatch(Rect.Width + 94, Rect.Height + 32);
+
+            using (Graphics graph = Graphics.FromImage(bmp))
+            {
+                graph.FillRectangle(selectionBrush, new Rectangle(pointStart, new Size(endPoint)));
+            }
+            pictureBox1.Image = bmp;
         }
 
         private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
@@ -138,9 +125,10 @@ namespace University_Schedule
 
         private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
         {
-            // Determine the initial rectangle coordinates...
             RectStartPoint = e.Location;
+            isDrawing = true;
             indexPoint = 0;
+            pointStart = Drawing.SearchForMatch(RectStartPoint.X, RectStartPoint.Y);
             point[indexPoint] = Drawing.SearchForMatch(RectStartPoint.X,RectStartPoint.Y);
             Invalidate();
         }
@@ -150,25 +138,17 @@ namespace University_Schedule
         private void pictureBox1_Paint(object sender, PaintEventArgs e)
         {
            
-            // Draw the rectangle...
             if (pictureBox1.Image != null)
             {
-                if (Rect != null && Rect.Width > 0 && Rect.Height > 0)
+                if (Rect != null && Rect.Width > 0 && Rect.Height > 0 && isDrawing)
                 {
-                    Point point1 = Drawing.SearchForMatch(Rect.Location.X, Rect.Location.Y);
                     Point point2 = Drawing.SearchForMatch(Rect.Size.Width + 94, Rect.Size.Height + 32);
                     Size size = new Size(point2);
-                    selected_rectagle = new Rectangle(point1, size);
-                     e.Graphics.FillRectangle(selectionBrush, selected_rectagle);
-
-                    if (point1!=point[indexPoint])
-                    {
-                        indexPoint++;
-                        point[indexPoint] = point1;
-                    }
-
+                    selected_rectagle = new Rectangle(new Point(Rect.Location.X, Rect.Location.Y), new Size(Rect.Size.Width, Rect.Size.Height));
+                     e.Graphics.FillRectangle(selectionBrush, Rect);
                 }
             }
+          
         }
     }
 }
